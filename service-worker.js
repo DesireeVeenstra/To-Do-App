ee0be56b31f5f96b7aa8dae5459eb154e02748be
DesireeVeenstra <dsmveenstra@gmail.com>
2025-02-05 07:48:26 +0000

    service change

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 const CACHE_NAME = 'to-do-pwa-cache-v1';
 const FILES_TO_CACHE = [
-    '/to-do-app',
-    '/to-do-app/index.html',
-    '/to-do-app/style.css',
-    '/to-do-app/app.js',
-    '/to-do-app/manifest.json',
-    '/to-do-app/icons/icon-128.png',
-    '/to-do-app/icons/icon-512.png'
+    '/YOUR-REPOSITORY-NAME',
+    '/YOUR-REPOSITORY-NAME/index.html',
+    '/YOUR-REPOSITORY-NAME/style.css',
+    '/YOUR-REPOSITORY-NAME/app.js',
+    '/YOUR-REPOSITORY-NAME/manifest.json',
+    '/YOUR-REPOSITORY-NAME/icons/icon-128.png',
+    '/YOUR-REPOSITORY-NAME/icons/icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
